e30a032f5242e5fc42c807f89dd4936689d766c3
RamonRauber <162449700+RamonRauber@users.noreply.github.com>
2025-02-24 08:46:55 -0300

    Add files via upload

---
+++ boolean.js
@@ -0,0 +1,18 @@
+// Operadores de comparação
+//Operador de igualdade (compara apenas o valor)
+console.log("1"  == 1)
+console.log(true  == 1)
+console.log(false  == 0)
+console.log(false =="")
+
+// Opedador de igualdade estrita
+// (compara valor e tipo)
+console.log("1"  === 1)
+console.log(true  === 1)
+console.log(false  === 0)
+
+//Verificar tipo da variável
+console.log(typeof("1"))  // -> texto (string)
+console.log(typeof(1))  // -> number
+console.log(typeof(true))  // -> boolean
+
