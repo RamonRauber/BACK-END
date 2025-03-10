42710d5e1fd3c7b09c98a1cf44f98802d0263e20
RamonRauber <162449700+RamonRauber@users.noreply.github.com>
2025-03-10 08:16:03 -0300

    Add files via upload

--- exercicio1.js
+++ exercicio1.js
@@ -1,4 +1,17 @@
 let peso = 65
 
 let qtd_agua = peso * 35 / 1000
+
 console.log("Quantidade de Água =  " + qtd_agua )
+
+
+const av1=10.0
+const av2=7.0
+const av3=4.0
+const media= (av1+av2+av3)/3
+if (media>=6.0){
+    console.log("Aprovado");
+} else{
+    console.log ("Reprovado");
+}
+console.log("Média de "+media)
