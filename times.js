8883bc768047c26425eb80cc7318ebf10c7e8295
RamonRauber <162449700+RamonRauber@users.noreply.github.com>
2025-04-07 08:35:19 -0300

    Add files via upload

---
+++ times.js
@@ -0,0 +1,62 @@
+let times= [
+    {
+        nome: "Magno",
+        goleiro: "André Deko",
+        alaDireita: "Dieguinho",
+        alaEsquerda: "Leandro Lino",
+        pivo: "Leozin",
+        fixo: "Rodrigo",
+        técnico: "Douglão",
+        vitorias: 31,
+        derrotas: 5,
+
+    },
+    {
+        nome: "Corinthians",
+        goleiro: "Koji",
+        alaDireita: "Daniel",
+        alaEsquerda: "Edimar",
+        pivo: "Gugu Flores",
+        fixo: "Sinésio",
+        técnico: "Malafaia",
+        vitorias: 18,
+        derrotas: 15,
+
+    },
+    {
+        nome: "Santo André",
+        goleiro: "Caio",
+        alaDireita: "Xaropinho",
+        alaEsquerda: "Gui Henriques",
+        pivo: "Cauê",
+        fixo: "Felipe",
+        técnico: "Cidão",
+        vitorias: 20,
+        derrotas: 19,
+
+    },
+    {
+        nome: "Joinville",
+        goleiro: "Kleyton",
+        alaDireita: "Alex",
+        alaEsquerda: "Lucas Cézar",
+        pivo: "Dieguinho",
+        fixo: "Ernani",
+        técnico: "Herick",
+        vitorias: 31,
+        derrotas: 4,
+
+    },
+    {
+        nome: "ACBF",
+        goleiro: "Ângelo",
+        alaDireita: "Marcolla",
+        alaEsquerda: "Alves",
+        pivo: "Camilo",
+        fixo: "Daniel",
+        técnico: "Sandro",
+        vitorias: 24,
+        derrotas: 11,
+
+    }
+]
